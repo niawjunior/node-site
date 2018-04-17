04cfdcc1c80ecb66c3e915a08674704a1de013cf
niawjunior <pasupol@dome.cloud>
2018-04-18 01:05:52 +0700

    create server using express :heavy_plus_sign:

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-//import the http module
-var http = require('http');
+// //import the http module
+// var http = require('http');
 
-//handle sending requests and returning responses
-function handleRequests(req, res) {
-  res.end('Hello world');
-}
+// //handle sending requests and returning responses
+// function handleRequests(req, res) {
+//   res.end('Hello world');
+// }
 
-var server = http.createServer(handleRequests);
+// var server = http.createServer(handleRequests);
 
-//start server and listen on port 3000
-server.listen(3000, function() {
-  console.log('Listening on port 3000');
+// //start server and listen on port 3000
+// server.listen(3000, function() {
+//   console.log('Listening on port 3000');
+// })
+
+////////////// EXPRESS ////////////////
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function(req, res) {
+  res.send('HELLO WORLD');
+})
+app.listen(port, function() {
+  console.log('app listening')
 })
